cdc26fcc6fe924a6dcdba316d76618793b132eec
ninazmul <nazmulsaw@gmail.com>
2024-02-12 14:48:12 +0600

    pull code, cause have some issue

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -105,7 +105,7 @@ const About = () => {
           >
             <a
               data-aos="fade-right"
-              href="https://drive.google.com/file/d/1NzJUC53OkBVPZLQPKhZeUJIjTkQg85M2/view?usp=sharing"
+              href="https://drive.google.com/file/d/1aihDBamIngdPD7HTmNsYOysAdE4E3ay4/view?usp=sharing"
               className="btn btn-outline font-bold shadow-xl hover:shadow-fuchsia-800/50 border-2 hover:bg-fuchsia-500 border-fuchsia-800 rounded-lg uppercase relative overflow-hidden text-center my-1 text-xl w-72 text-white hover:text-none"
             >
               Download RESUME{" "}
@@ -115,7 +115,7 @@ const About = () => {
             </a>
             <a
               data-aos="fade-left"
-              href="https://drive.google.com/file/d/1U2xgiPDHVTLAG7yZ4_esS3Vc9GUQIaI9/view?usp=sharing"
+              href="https://drive.google.com/file/d/1OnaGGzJmm_-2nOhGoPZlBsCeDjiWfz28/view?usp=sharing"
               className="btn btn-outline font-bold shadow-xl hover:shadow-fuchsia-800/50 border-2 hover:bg-fuchsia-500 border-fuchsia-800 rounded-lg uppercase relative overflow-hidden text-center my-1 text-xl w-72 text-white hover:text-none"
             >
               Download CV{" "}
--- src/Pages/Home/AutoScrollReviews.jsx
+++ src/Pages/Home/AutoScrollReviews.jsx
@@ -1,24 +1,89 @@
 
-import Marquee from "react-fast-marquee";
+import { useRef, useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
 import ReviewsScroll from "../Portfolio/Review/ReviewsScroll";
 import { Link } from "react-router-dom";
 
 const AutoScrollReviews = () => {
+      const sliderRef = useRef(null);
+      const [slidesToShow, setSlidesToShow] = useState(4);
+
+      const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        swipeToSlide: true,
+        focusOnSelect: false,
+        slidesToShow: slidesToShow,
+      };
+
+      const updateSlidesToShow = () => {
+        const windowWidth = window.innerWidth;
+        if (windowWidth >= 1024) {
+          setSlidesToShow(4);
+        } else if (windowWidth >= 768) {
+          setSlidesToShow(3);
+        } else {
+          setSlidesToShow(2);
+        }
+      };
+
+      const goToNext = () => {
+        sliderRef.current.slickNext();
+      };
+
+      const goToPrev = () => {
+        sliderRef.current.slickPrev();
+      };
+
+      useEffect(() => {
+        updateSlidesToShow(); // Initial calculation
+
+        // Update slidesToShow on window resize
+        window.addEventListener("resize", updateSlidesToShow);
+
+        // Cleanup event listener on component unmount
+        return () => {
+          window.removeEventListener("resize", updateSlidesToShow);
+        };
+      }, []);
 
   return (
     <div className="my-10" data-aos="fade-left">
       <h1 className="text-3xl md:text-5xl font-bold leading-normal text-center uppercase py-6">
         Client's <span className="text-fuchsia-500">Reviews</span>
       </h1>
-      <Marquee speed={30} className="gap-4">
-        {/* Create a gap at the beginning of the Marquee */}
-        <div className="ml-2"></div>
-        <div>
-          <ReviewsScroll></ReviewsScroll>
+      <div className="relative">
+        <Slider ref={sliderRef} {...settings}>
+          <div>
+            <ReviewsScroll></ReviewsScroll>
+          </div>
+        </Slider>
+
+        <div className="absolute top-1/2 left-5 transform -translate-y-1/2 flex space-x-4 z-10">
+          <button
+            className="bg-gray-500 text-dark hover:text-white p-2 text-2xl glass hover:bg-black rounded-lg"
+            onClick={goToPrev}
+          >
+            <IoIosArrowBack />
+          </button>
         </div>
-        {/* Create a gap at the end of the Marquee */}
-        <div className="mr-2"></div>
-      </Marquee>
+        <div className="absolute top-1/2 right-5 transform -translate-y-1/2 flex space-x-4 z-10">
+          <button
+            className="bg-gray-500 text-dark hover:text-white p-2 text-2xl glass hover:bg-black rounded-lg"
+            onClick={goToNext}
+          >
+            <IoIosArrowForward />
+          </button>
+        </div>
+      </div>
       <Link to="/dashboard/addReview" className="flex justify-center">
         <button className="btn btn-outline m-4 border-t-0 border-x-0 border-b-4 neno-button shadow-xl hover:shadow-fuchsia-800/50 border-2 hover:bg-fuchsia-500 border-fuchsia-800 rounded-lg uppercase relative overflow-hidden text-center font-extrabold text-white">
           Give Review
