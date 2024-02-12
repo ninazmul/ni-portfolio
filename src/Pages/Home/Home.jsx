3870d7b1946bdf7c5c2b9b9e0a5fe19c9bd48789
ninazmul <nazmulsaw@gmail.com>
2024-02-12 14:52:31 +0600

    pull code, cause have some issue

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,4 @@
 import ContactUs from "../Contact/ContactUs";
-import AutoScrollReviews from "./AutoScrollReviews";
 import Header from "./Header";
 import Services from "./Services";
 import Skills from "./Skills/Skills";
@@ -10,7 +9,6 @@ const Home = () => {
             <Header></Header>
             <Skills></Skills>
             <Services></Services>
-            <AutoScrollReviews></AutoScrollReviews>
             <ContactUs></ContactUs>
         </div>
     );
